f5ae6776ceaa07bee282e210b778b7bac3ec16be
kjuulh <contact@kjuulh.io>
2024-04-21 22:32:14 +0200

    feat: add base app
    Signed-off-by: kjuulh <contact@kjuulh.io>

---
+++ sample-app/src/main.rs
@@ -0,0 +1,80 @@
+use std::{collections::HashMap, net::SocketAddr};
+
+use axum::{routing::get, Router};
+use clap::{Parser, Subcommand, ValueEnum};
+use serde::Deserialize;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let cmd = Command::parse();
+
+    match cmd.commands {
+        Commands::HelloWorld { format } => match format {
+            OutputFormat::Text => println!("Hello, world!"),
+            OutputFormat::Json => println!(
+                "{}",
+                serde_json::json!({
+                    "message": "Hello, world!",
+                })
+            ),
+        },
+        Commands::Fetch {} => {
+            #[derive(Deserialize, Clone, Debug)]
+            struct HttpResp {
+                headers: HashMap<String, String>,
+            }
+
+            let resp: HttpResp = reqwest::get("https://httpbin.org/get")
+                .await?
+                .json()
+                .await?;
+
+            println!("httpbin responded with headers: {}", resp.headers.len());
+        }
+        Commands::Serve { host } => {
+            let app = Router::new().route("/", get(|| async move { "Hello, world!" }));
+
+            tracing::info!("listening on {}", &host);
+            let listener = tokio::net::TcpListener::bind(&host).await?;
+            axum::serve(listener, app.into_make_service()).await?;
+        }
+        Commands::LocalRepo {} => {
+            let repo = git2::Repository::open(".")?;
+
+            for remote in repo.remotes()?.iter().flatten() {
+                println!("found remote: {}", remote);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about, long_about = None, subcommand_required = true)]
+struct Command {
+    #[command(subcommand)]
+    commands: Commands,
+}
+
+#[derive(Clone, Debug, Subcommand)]
+enum Commands {
+    HelloWorld {
+        #[arg(long = "format", default_value = "text")]
+        format: OutputFormat,
+    },
+    Fetch {},
+    LocalRepo {},
+    Serve {
+        #[arg(long = "host", default_value = "127.0.0.1:3000")]
+        host: SocketAddr,
+    },
+}
+
+#[derive(ValueEnum, Clone, Debug)]
+enum OutputFormat {
+    Text,
+    Json,
+}
